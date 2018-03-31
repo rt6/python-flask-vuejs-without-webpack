8e0f42c4292b6337488b407dc3195f079574d520
rt6 <rt6@users.noreply.github.com>
2018-03-31 15:37:02 +1100

    use vue single file components without webpack

---
+++ web-vuejs-client/vue-single-file-component-without-webpack/my-component.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="hello"><h1>Hello {{who}}</h1></div>
+</template>
+
+<script>
+module.exports = {
+    data: function() {
+        return {
+            who: 'world to all'
+        }
+    }
+}
+</script>
+
+<style>
+.hello {
+    background-color: #74b9ff;
+}
+</style>
